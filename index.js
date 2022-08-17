52f77f6b00130c062f60dea6802d602b96fb06b0
Mayur Saroj <mayursarojdev@gmail.com>
2022-08-17 15:57:42 +0530

    :rocket: Handle cors
    install cors package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const adsRoute = require("./routes/ads");
@@ -7,12 +8,14 @@ const companiesRoute = require("./routes/companies");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors({ origin: "*" }));
 
 mongoose
   .connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
   .then(() => console.log("Connected to DB!"))
   .catch((err) => console.log(err));
 
+// route handling
 app.use("/api/ads", adsRoute);
 app.use("/api/companies", companiesRoute);
 
